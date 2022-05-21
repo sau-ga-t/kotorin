b7a7fea1e8734432c5fecf1d6c2e9439381185db
Saugat Poudel <poudelsaugat45@gmail.com>
2022-05-21 11:57:52 +0630

    Initial commit

---
+++ src/main/kotlin/com/drago/Application.kt
@@ -0,0 +1,11 @@
+package com.drago
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.drago.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
